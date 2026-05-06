bbb068a2148b72905541f067be62ab21c28d5ca5
israelsilva79 <israelsilva79.dev@gmail.com>
2026-05-06 14:14:26 -0300

    config

--- gestao_eventos/settings.py
+++ gestao_eventos/settings.py
@@ -1,12 +1,7 @@
 import os
 from pathlib import Path
 
-# Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-
-
-# Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 SECRET_KEY = 'django-insecure-!_substitua_por_uma_chave_real_!'
 
@@ -61,9 +56,6 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'gestao_eventos.wsgi.application'
 
-
-# Database
-# https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
 DATABASES = {
     'default': {
