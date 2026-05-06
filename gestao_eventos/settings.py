a83f8bac215139b374a185cf06de87133389a452
israelsilva79 <israelsilva79.dev@gmail.com>
2026-05-06 11:12:10 -0300

    requirements e gitignore

--- gestao_eventos/settings.py
+++ gestao_eventos/settings.py
@@ -12,7 +12,7 @@ SECRET_KEY = 'django-insecure-!_substitua_por_uma_chave_real_!'
 
 DEBUG = True
 
-ALLOWED_HOSTS = ['0.0.0.0']
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -107,12 +107,13 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
-STATIC_URL = 'static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_URL = '/static/'
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'templates/static')]
+STATIC_ROOT = os.path.join('static')
 
 # Media files (uploads)
-MEDIA_URL = 'media/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join('media')
 
 
 
